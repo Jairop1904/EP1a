86b52b36e582d9e99e236dacce52c0b407cdaad6
Jairo Pulgarin <40905226+Jairop1904@users.noreply.github.com>
2019-05-19 18:05:18 -0500

    segunda vez

--- app/src/main/java/com/uisrael/ep1a/MainActivity.java
+++ app/src/main/java/com/uisrael/ep1a/MainActivity.java
@@ -10,8 +10,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int and;
-        int dos;
+        int and=0;
+        int dos=0;
         int tres;
+        if(and!=1){
+            dos=1;
+        }else {
+            tres=0;
+        }
     }
 }
